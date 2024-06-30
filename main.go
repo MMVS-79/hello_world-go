da404287e73c54dcc5c4634947bfffeb78026f56
MMVS <zeinny1997@gmail.com>
2024-06-30 16:33:01 -0600

    reorganize

--- main.go
+++ main.go
@@ -4,30 +4,10 @@ import (
 	"net/http"
 	"fmt"
 	//"errors"
-	"html/template"
 )
 
 const portNum = ":8080"
 
-//Home is the home page handler 
-func Home(w http.ResponseWriter, r *http.Request){
-	renderTemplate(w, "home.page.tmpl")
-}
-
-
-// About is the about page handler
-func About(w http.ResponseWriter, r *http.Request){
-	renderTemplate(w, "about.page.tmpl")
-}
-
-func renderTemplate(w http.ResponseWriter, tmpl string){
-	parsedTemplate,_:=template.ParseFiles("./templates/" + tmpl)
-	err := parsedTemplate.Execute(w,nil)
-	if err != nil {
-		fmt.Println("error parsing template", err)
-		return
-	}
-}
 
 func main() {
 
