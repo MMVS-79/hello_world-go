5156e32a70c724520b73b5487b99dc4b5f5e1fe2
MMVS <zeinny1997@gmail.com>
2024-07-06 20:45:06 -0600

    refactored caching

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -4,6 +4,9 @@ import (
 	"net/http"
 	"fmt"
 	"myapp/pkg/handlers"
+	"myapp/pkg/config"
+	"myapp/pkg/render"
+	"log"
 	//"errors"
 )
 
@@ -12,9 +15,24 @@ const portNum = ":8080"
 
 func main() {
 
+	var app config.AppConfig
 
-	http.HandleFunc("/", handlers.Home)
-	http.HandleFunc("/about", handlers.About)
+	tc, err := render.CreateTemplateCache()
+	if err != nil {
+		log.Fatal("cannot create template cache")
+	}
+
+	app.TemplateCache = tc
+	app.UseCache = false
+
+	repo := handlers.NewRepo(&app)
+	handlers.NewHandlers(repo)
+
+	render.NewTemplates(&app)
+
+
+	http.HandleFunc("/", handlers.Repo.Home)
+	http.HandleFunc("/about", handlers.Repo.About)
 
 	fmt.Println(fmt.Sprintf("Starting application on port %s", portNum))
 	_ = http.ListenAndServe(portNum, nil)
